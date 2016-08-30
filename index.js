80c75d50ed0a7f5980507521d2784a9043f32ad0
Seth Holladay <me@seth-holladay.com>
2016-08-29 23:11:26 -0400

    New feature: get data from most recent build

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 'use strict';
 
+const fs = require('fs');
+const path = require('path');
 const branchName = require('branch-name');
 const buildVersion = require('build-version');
+
+const realpath = (fp) => {
+    return new Promise((resolve, reject) => {
+        fs.realpath(fp, (err, resolvedPath) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve(resolvedPath);
+        });
+    });
+};
 
 const buildData = (option) => {
     const config = Object.assign({}, option);
@@ -15,6 +29,31 @@ const buildData = (option) => {
                 version : data[1]
             };
         });
+};
+
+buildData.latest = (option) => {
+    const config = Object.assign(
+        {
+            cwd : ''
+        },
+        option
+    );
+
+    if (config.branch && config.version) {
+        return Promise.resolve({
+            branch  : config.branch,
+            version : config.version
+        });
+    }
+
+    const linkPath = config.branch ? path.join('build', config.branch, 'latest') : 'latest-build';
+
+    return realpath(path.join(config.cwd, linkPath)).then((resolvedPath) => {
+        return {
+            branch  : config.branch || path.basename(path.join(resolvedPath, '..')),
+            version : config.version || path.basename(resolvedPath)
+        };
+    });
 };
 
 module.exports = buildData;
